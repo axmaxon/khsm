31457be8bd45b464e37dd991486bd96845015510
axmaxon <axmaxon@gmail.com>
2021-07-12 13:07:43 +0500

    test: Game#take_money and Game#status

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Game, type: :model do
   let(:game_w_questions) { FactoryBot.create(:game_with_questions, user: user) }
 
   # Группа тестов на работу фабрики создания новых игр
-  context 'Game Factory' do
+  describe 'Game Factory' do
     it 'Game.create_game! new correct game' do
       # генерим 60 вопросов с 4х запасом по полю level,
       # чтобы проверить работу RANDOM при создании игры
@@ -34,13 +34,14 @@ RSpec.describe Game, type: :model do
       expect(game.status).to eq(:in_progress)
       # проверяем корректность массива игровых вопросов
       expect(game.game_questions.size).to eq(15)
-      expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+      expect(game.game_questions.map(&:level)).to eq((0..14).to_a)
     end
+
+
   end
 
-
   # тесты на основную игровую логику
-  context 'game mechanics' do
+  describe 'game mechanics' do
 
     # правильный ответ должен продолжать игру
     it 'answer correct continues game' do
@@ -59,6 +60,59 @@ RSpec.describe Game, type: :model do
       # игра продолжается
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
+    end
+
+    it 'Game.take_money!' do
+      # Состояние игры: в процессе, получаем очередной(текущий) вопрос
+      next_question = game_w_questions.current_game_question
+
+      # Получаем правильный ответ на текущий вопрос
+      game_w_questions.answer_current_question!(next_question.correct_answer_key)
+
+      # Игрок берёт деньги
+      game_w_questions.take_money!
+
+      # Проверяем наличие денежного приза в базе
+      award = game_w_questions.prize
+      expect(award).to be > 0
+
+      # Проверка статуса игры (должен соответствовать :money)
+      expect(game_w_questions.status).to eq :money
+
+      # Проверка что игра завершена
+      expect(game_w_questions.finished?).to be_truthy
+
+      # Проверка баланса пользователя
+      expect(user.balance).to eq award
+    end
+  end
+
+  describe 'Game.status' do
+
+    # перед каждым тестом "завершаем игру"
+    before(:each) do
+      game_w_questions.finished_at = Time.now
+      expect(game_w_questions.finished?).to be_truthy
+    end
+
+    it ':won' do
+      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+      expect(game_w_questions.status).to eq(:won)
+    end
+
+    it ':fail' do
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:fail)
+    end
+
+    it ':timeout' do
+      game_w_questions.created_at = 1.hour.ago
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:timeout)
+    end
+
+    it ':money' do
+      expect(game_w_questions.status).to eq(:money)
     end
   end
 end
